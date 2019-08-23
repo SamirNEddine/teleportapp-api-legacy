e37c5ee8847cffcf2f1bff2c25a503f6d6f6b049
Samir <samir@nevereatalone.io>
2019-08-24 00:08:09 +0200

    add nested queries

--- graphql/User/type/nestedResolvers.js
+++ graphql/User/type/nestedResolvers.js
@@ -1,16 +1,40 @@
+const Company = require('../../../mongo/models/Company');
 
-module.exports.nestedUserCompanyResolver = function () {
-    
+module.exports.nestedUserCompanyResolver = async function (user) {
+    try{
+        return await Company.findById(user.companyId);
+    }catch (error) {
+        console.debug(error);
+        throw(error);
+    }
 };
 
-module.exports.nestedUserDepartmentResolver = function () {
-
+module.exports.nestedUserDepartmentResolver = async function (user) {
+    try{
+        const company = await Company.findById(user.companyId);
+        return company.departments.find(dep => user.departmentId == dep.id);
+    }catch (error) {
+        console.debug(error);
+        throw(error);
+    }
 };
 
-module.exports.nestedUserSiteResolver = function () {
-    
+module.exports.nestedUserSiteResolver = async function (user) {
+    try{
+        const company = await Company.findById(user.companyId);
+        return company.sites.find(site => user.siteId == site.id);
+    }catch (error) {
+        console.debug(error);
+        throw(error);
+    }
 };
 
-module.exports.nestedUserTeamResolver = function () {
-
+module.exports.nestedUserTeamResolver = async function (user) {
+    try{
+        const company = await Company.findById(user.companyId);
+        return company.teams.find(team => user.teamId == team.id);
+    }catch (error) {
+        console.debug(error);
+        throw(error);
+    }
 };
