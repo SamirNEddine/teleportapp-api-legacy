9a5ed3f5827cd18c6cfcd3d18af4a22f013a3cf9
Samir <samir@nevereatalone.io>
2019-08-23 23:54:30 +0200

    add user resolver

--- graphql/User/queries/resolvers.js
+++ graphql/User/queries/resolvers.js
@@ -2,7 +2,8 @@ const User = require('../../../mongo/models/User');
 
 module.exports.userResolver = async function (_, {id}, context) {
     try{
-
+        const user = await User.findById(id);
+        return {...user._doc, password: ''}
     }catch(error){
         console.error(error);
         throw(error);
