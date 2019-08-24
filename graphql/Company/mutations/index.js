52ea58125a5bf236b343766dfe49e58daf3f56e1
Samir <samir@nevereatalone.io>
2019-08-24 14:28:37 +0200

    Add authorization helpers

--- graphql/Company/mutations/index.js
+++ graphql/Company/mutations/index.js
@@ -1,12 +1,13 @@
 const { CompanyType, inputFields } = require('../type');
 const { createCompanyResolver } = require('./resolvers');
 const { authenticatedResolver } = require('../../../utils/authentication');
+const { authorizedResolver, AccessLevels } = require('../../../utils/authorization');
 
 /** Mutations definitions **/
 const createCompany = {
     type: CompanyType,
     args: inputFields.createCompany,
-    resolve: authenticatedResolver(createCompanyResolver)
+    resolve: authorizedResolver(authenticatedResolver(createCompanyResolver), AccessLevels.SUPER_ADMIN)
 };
 
 /** Exports **/
