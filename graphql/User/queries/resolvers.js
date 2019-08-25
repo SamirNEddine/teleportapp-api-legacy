9a032fe32ae8c4740d691b3be7ad4afd10516f19
Samir <samir@nevereatalone.io>
2019-08-25 14:29:33 +0200

    add users graphql queries

--- graphql/User/queries/resolvers.js
+++ graphql/User/queries/resolvers.js
@@ -10,3 +10,16 @@ module.exports.userResolver = async function (_, {id}, {user}) {
         throw(error);
     }
 };
+module.exports.usersResolver = async function (_, {companyId}, {user}) {
+    try{
+        companyId = companyId ? companyId : user.companyId;
+        const users = await User.find({companyId});
+        return users.map( u => {
+            u.password = '';
+            return u;
+        });
+    }catch(error){
+        console.error(error);
+        throw(error);
+    }
+};
