f3f1fc5d61a42a9f174d750a78013c2693631dad
Samir <samir@nevereatalone.io>
2019-09-04 19:42:49 +0200

    add tokbox helper

---
+++ utils/openTok.js
@@ -0,0 +1,19 @@
+const OpenTok = require('opentok');
+
+const openTok = new OpenTok(process.env.OPEN_TOK_API_KEY, process.env.OPEN_TOK_API_SECRET);
+
+module.exports.createNewSession = function(){
+    return new Promise(function(resolve, reject){
+        openTok.createSession(function(err, session){
+            if(err){
+                reject(err);
+            }else{
+                resolve(session.sessionId);
+            }
+        });
+    });
+};
+
+module.exports.generateTokenForSession = function(sessionId){
+    return openTok.generateToken(sessionId);
+};
