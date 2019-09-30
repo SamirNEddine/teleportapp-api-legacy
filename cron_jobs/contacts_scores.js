9d36f6ec76381a315436696f50513e8da8dc7580
Samir <samir@nevereatalone.io>
2019-09-30 14:09:46 +0200

    first score algorithm

--- cron_jobs/contacts_scores.js
+++ cron_jobs/contacts_scores.js
@@ -4,9 +4,14 @@ const { BigQuery } = require('@google-cloud/bigquery');
 
 const bigQuery = new BigQuery();
 
-async function conversationsQuery() {
-    const query = `SELECT conversation_id, user_id
-      FROM \`api_dev.start_conversation_view\``;
+const ADD_CONTACT_WEIGHT = 100;
+const ADD_BY_CONTACT_WEIGHT = 20;
+
+async function addContactQuery() {
+    const query = `SELECT COUNT(conversation_id) as conversations, contact_id as contact, user_id as user
+      FROM \`api_dev.add_contact_view\` as ADD_CONTACT
+      GROUP BY user, contact
+      ORDER BY conversations DESC`;
     const options = {
         query: query
     };
@@ -14,11 +19,33 @@ async function conversationsQuery() {
     console.log(`Job ${job.id} started.`);
 
     const [rows] = await job.getQueryResults();
-    console.log('Rows:');
-    rows.forEach(row => console.log(row));
+    return rows;
+}
+
+async function updateContactsScore(){
+    const addContactQueryResults = await addContactQuery();
+    console.log(addContactQueryResults);
+    const users = {};
+
+    const addContactScore = function(contactId, userId, score){
+        if (!users[userId]) users[userId] = [];
+        const contacts = users[userId];
+        const contactIndex = contacts.findIndex(c => c.contactId === contactId);
+        const contact = contactIndex !== -1 ? contacts[contactIndex] : {contactId: contactId, score: 0};
+        contact.score += score;
+        contactIndex !== -1 ? contacts[contactIndex] = contact : contacts.push(contact);
+    };
+
+    addContactQueryResults.forEach( row => {
+        //Score part 1: Contacts added by the user
+        addContactScore(String(row.contact), String(row.user), ADD_CONTACT_WEIGHT*row.conversations);
+        //Score part 2: Contact who add the user
+        addContactScore(String(row.user), String(row.contact), ADD_BY_CONTACT_WEIGHT*row.conversations);
+    });
+    console.log('Add Contact users\n', users);
 }
 
 function main() {
-    conversationsQuery();
+    updateContactsScore();
 }
 main(...process.argv.slice(2));
