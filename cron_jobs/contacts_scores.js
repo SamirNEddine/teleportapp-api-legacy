8d20f4e3a40a4943311a959bd7e820fc2771b179
Samir <samir@nevereatalone.io>
2019-09-30 16:01:30 +0200

    use user scores and update recommended contacts

--- cron_jobs/contacts_scores.js
+++ cron_jobs/contacts_scores.js
@@ -1,4 +1,5 @@
-const { connectToDb } = require('../utils/mongoose');
+require('dotenv').config();
+const { connectToDb, disconnectFromDb } = require('../utils/mongoose');
 const User = require('../mongo/models/User');
 const { BigQuery } = require('@google-cloud/bigquery');
 
@@ -23,29 +24,55 @@ async function addContactQuery() {
 }
 
 async function updateContactsScore(){
-    const addContactQueryResults = await addContactQuery();
-    console.log(addContactQueryResults);
-    const users = {};
+    try{
+        const addContactQueryResults = await addContactQuery();
+        console.log(addContactQueryResults);
+        const users = {};
 
-    const addContactScore = function(contactId, userId, score){
-        if (!users[userId]) users[userId] = [];
-        const contacts = users[userId];
-        const contactIndex = contacts.findIndex(c => c.contactId === contactId);
-        const contact = contactIndex !== -1 ? contacts[contactIndex] : {contactId: contactId, score: 0};
-        contact.score += score;
-        contactIndex !== -1 ? contacts[contactIndex] = contact : contacts.push(contact);
-    };
+        const addContactScore = function(contactId, userId, score){
+            if (!users[userId]) users[userId] = [];
+            const contacts = users[userId];
+            const contactIndex = contacts.findIndex(c => c.contactId === contactId);
+            const contact = contactIndex !== -1 ? contacts[contactIndex] : {contactId: contactId, score: 0};
+            contact.score += score;
+            contactIndex !== -1 ? contacts[contactIndex] = contact : contacts.push(contact);
+        };
 
-    addContactQueryResults.forEach( row => {
-        //Score part 1: Contacts added by the user
-        addContactScore(String(row.contact), String(row.user), ADD_CONTACT_WEIGHT*row.conversations);
-        //Score part 2: Contact who add the user
-        addContactScore(String(row.user), String(row.contact), ADD_BY_CONTACT_WEIGHT*row.conversations);
-    });
-    console.log('Add Contact users\n', users);
+        addContactQueryResults.forEach( row => {
+            //Score part 1: Contacts added by the user
+            addContactScore(String(row.contact), String(row.user), ADD_CONTACT_WEIGHT*row.conversations);
+            //Score part 2: Contact who add the user
+            addContactScore(String(row.user), String(row.contact), ADD_BY_CONTACT_WEIGHT*row.conversations);
+        });
+        console.log('New scores\n', users);
+
+        await connectToDb();
+        console.log('Update recommended users in the database');
+        for(let userId in users){
+            if (users.hasOwnProperty(userId)){
+                let contacts = users[userId];
+                contacts.sort( (c1, c2) => {
+                    return c1.score < c2.score ? 1 : -1;
+                });
+                const user = await User.findById(userId);
+                if(user){
+                    user.recommendedContacts = contacts.map( c => (c.contactId));
+                    await user.save();
+                }else{
+                    console.warn(`User ${userid} not found in the database`);
+                }
+            }
+        }
+        disconnectFromDb();
+    }catch(e){
+        console.error(e);
+        disconnectFromDb();
+    }
+
 }
 
 function main() {
     updateContactsScore();
 }
+
 main(...process.argv.slice(2));
--- utils/mongoose.js
+++ utils/mongoose.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 const dbConnectURL = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_BASE_URL}:${process.env.DB_PORT}/${process.env.MAIN_DB_NAME}`;
 module.exports.connectToDb = function () {
-    mongoose.connect(dbConnectURL, {useNewUrlParser: true, useFindAndModify: false, useCreateIndex:true}).then(function(){
-        console.info('Successfully connected to the database: ' + process.env.MAIN_DB_NAME + ' using username: '+process.env.DB_USERNAME + ' on port: '+process.env.DB_PORT);
-    }).catch(function (error) {
-        console.error(error);
+    return new Promise(async function (resolve, reject) {
+        try{
+            await mongoose.connect(dbConnectURL, {useNewUrlParser: true, useFindAndModify: false, useCreateIndex:true});
+            console.info('Successfully connected to the database: ' + process.env.MAIN_DB_NAME + ' using username: '+process.env.DB_USERNAME + ' on port: '+process.env.DB_PORT);
+            resolve();
+        }catch(error){
+            console.error(error);
+            reject(error);
+        }
     });
-
+};
+module.exports.disconnectFromDb = function () {
+    mongoose.disconnect();
 };
